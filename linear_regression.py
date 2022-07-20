c83558fcb28c6cd89f6c283abecd7824eba32e2a
Usuario <andresroliveira98@gmail.com>
2022-07-20 13:04:38 -0300

    Update linear_regression.py

--- linear_regression.py
+++ linear_regression.py
@@ -1,14 +1,6 @@
 import numpy as np
 import pandas as pd
 
-data = pd.read_csv('diamonds.csv', index_col=0)
-
-y = data.pop('price')
-x = data
-
-print(x)
-print(y)
-
 # price : price in US dollars
 # carat : Carat weight of the diamond
 # cut : Describe cut quality of the diamond. ["Ideal", "Premium", "Very Good", "Good", "Fair"]
@@ -20,7 +12,47 @@ print(y)
 # y : width in mm
 # z : depth in mm
 
-for d in data:
-    arr = list(data[d])
-    if isinstance(arr[0], float):
-        print(np.sum(arr))
+cut = {
+    '"Ideal"': 5,
+    '"Premium"': 4,
+    '"Very Good"': 3,
+    '"Good"': 2,
+    '"Fair"': 1
+}
+color = {'"D"': 7, '"E"': 6, '"F"': 5, '"G"': 4, '"H"': 3, '"I"': 2, '"J"': 1}
+clarity = {
+    '"IF"': 8,
+    '"VVS1"': 7,
+    '"VVS2"': 6,
+    '"VS1"': 5,
+    '"VS2"': 4,
+    '"SI1"': 3,
+    '"SI2"': 2,
+    '"I1"': 1
+}
+
+# convert.csv collums with string into new_diamonds.csv collums floats
+
+# with open('diamonds.csv', 'r') as r, open('new_diamonds.csv', 'w') as w:
+#     rl = r.readline()
+#     wl = ','.join(rl.split(',')[1:])
+#     w.write(wl)
+#     rl = r.readline()
+#     cont = 0
+#     while rl.find(',') >= 0:
+#         wl = rl.split(',')[1:]
+#         wl[1] = str(cut[wl[1]])
+#         wl[2] = str(color[wl[2]])
+#         wl[3] = str(clarity[wl[3]])
+#         wl = ','.join(wl)
+#         w.write(wl)
+#         rl = r.readline()
+
+data = pd.read_csv('new_diamonds.csv', dtype=float)
+print(data)
+
+y_data = data.pop('price')
+x_data = data
+
+print(x_data.to_numpy())
+print(y_data.to_numpy())
