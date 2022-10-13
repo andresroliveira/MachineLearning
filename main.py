00933cbb9d9dc24af3731731adfd034dd5a64e78
Andrês <andresroliveira98@gmail.com>
2022-10-13 16:45:32 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
-import numpy as np
+import tensorflow as tf
 
-print('ola mundo')
+mnist = tf.keras.datasets.mnist
 
-f = 1
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+x_train, x_test = x_train / 255.0, x_test / 255.0
 
-for i in range(1, 5 + 1):
-    f *= i
+model = tf.keras.models.Sequential([
+    tf.keras.layers.Flatten(input_shape=(28, 28)),
+    tf.keras.layers.Dense(128, activation='relu'),
+    tf.keras.layers.Dropout(0.2),
+    tf.keras.layers.Dense(10, activation='softmax')
+])
 
-print(f)
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+model.fit(x_train, y_train, epochs=5)
+model.evaluate(x_test, y_test)
